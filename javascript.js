6caa1aa5a9c04b78feb8a2b2241d6afe1f9417d6
shaiksha7 <shaiksha1431813@gmail.com>
2024-04-19 12:06:55 +0530

    javascript

---
+++ javascript.js
@@ -0,0 +1,18 @@
+const decreseB =document.getElementById(`decreseB`);
+const resetB =document.getElementById(`resetB`);
+const increseB =document.getElementById(`increseB`);
+const count1 =document.getElementById(`count1`);
+let count= 0;
+increseB.onclick = function(){
+    count ++;
+   document.getElementById(`count1`).textContent =count
+}
+resetB.onclick = function(){
+    count=0;
+   document.getElementById(`count1`).textContent =count
+}
+decreseB.onclick = function(){
+    count --;
+  document.getElementById(`count1`).textContent =count
+}
+
